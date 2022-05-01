e91a253fe2e55cf299519c754154c1a579e55ecc
João Pedro Barros <jpedro@server01.foursys.local>
2022-05-01 02:37:24 -0300

    Testando e implementando navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import styles from '../../styles/components/Navbar.module.css'
+
+function Navbar(){
+    return(
+        <div className={styles.container}>
+
+            <a className={styles.item}>Home</a>
+            <a className={styles.item}>Home</a>
+        </div>
+    )
+}
+
+export default Navbar
