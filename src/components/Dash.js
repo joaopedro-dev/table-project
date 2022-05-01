9e901d0c8c776df5d131676f3e73d5b154e076b6
João Pedro Barros <jpedro@server01.foursys.local>
2022-05-01 12:03:48 -0300

    Colocando logo e estilizando itens da nav

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -4,20 +4,10 @@ function Dash() {
     return (
         <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello table!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import styles from '../../styles/components/Navbar.module.css'
+import Image from 'next/image'
 
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <div className={styles.container}>
+            <div className={styles.logo}>
+                <p className={styles.item_logo}>
+                    Table
+                </p>
+                <Image src="/octopus.png" alt="Logo" width={40} height={40} />
+            </div>
 
-            <a className={styles.item}>Home</a>
-            <a className={styles.item}>Home</a>
+            <div className={styles.item}>
+                <a>Home</a>
+                <a>About us</a>
+                <a>Cliente</a>
+                <a>Terceiro</a>
+                <a>Recado</a>
+            </div>
         </div>
     )
 }
