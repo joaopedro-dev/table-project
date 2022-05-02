a744d8182b54edc5678add35a49044c35e9b8946
João Pedro Barros <jpedro@server01.foursys.local>
2022-05-02 20:23:20 -0300

    adicionando cards na home

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,12 +5,24 @@ function Cards(){
     return (
         <div className={styles.grid}>
             <div className={styles.phrase}>
-                <h2 className={styles.title_phrase}>Table</h2>
+                <h2 className={styles.title}>Table</h2>
                 <Image src="/octopus.png" alt="Logo" width={95} height={95} />
-                <p className={styles.text_phrase}>
+                <p className={styles.text}>
                 “No meio da dificuldade, encontra-se a oportunidade”
                 </p>
                 <h3 className={styles.author_phrase}>Albert Einstein</h3>
+            </div>
+            <div className={styles.row}>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>Registrar Ligação</h1>
+                    <Image src="/hard-work.png" alt="Logo" width={95} height={95} />
+                    <p className={styles.text}>Tabular de forma rápida</p>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>Busque Evoluir</h1>
+                    <Image src="/magic-book.png" alt="Logo" width={95} height={95} />
+                    <p className={styles.text}>Manifesto table</p>
+                </div>
             </div>
         </div>
     )
