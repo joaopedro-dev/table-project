3b527e117ba8a3a5928e376d381c0d3200982d55
João Pedro Barros <jpedro@server01.foursys.local>
2022-05-03 17:04:04 -0300

    Ajustando responsividade

--- pages/card.js
+++ pages/card.js
@@ -1,89 +1,56 @@
 import styles from '../styles/Card.module.css'
 import Image from 'next/image'
+import Navbar from '../src/components/Navbar'
 
 export default function Home() {
   return (
         <div className={styles.container}>
-        <div className={styles.grid}>
-        <div className={styles.row}>
-                <div className={styles.card}>
-                    <h1 className={styles.title}>
-                        TITLE CARD
-                    </h1>
-                    <Image src="/octopus.png" alt="Logo" width={35} height={35} />
-                    <p className={styles.text}>
-                        Lorem
-                    </p>
-                    <h2 className={styles.author}>
-                        Naruto
-                    </h2>
+            <div className={styles.grid}>
+                <div className={styles.row}>
+                    <div className={styles.card}>
+                        <h1 className={styles.title}>Table</h1>
+                        <Image src="/octopus.png" alt="Logo" width={35} height={35} />
+                        <p className={styles.text}>
+                            Lorem
+                        </p>
+                        <h2 className={styles.author}>
+                            Naruto
+                        </h2>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.card}>
-                    <h1 className={styles.title}>
-                        TITLE CARD
-                    </h1>
-                    <Image src="/octopus.png" alt="Logo" width={35} height={35} />
-                    <p className={styles.text}>
-                        Lorem
-                    </p>
-                    <h2 className={styles.author}>
-                        Naruto
-                    </h2>
-                </div>
-                <div className={styles.card}>
-                    <h1 className={styles.title}>
-                        TITLE CARD
-                    </h1>
-                    <Image src="/octopus.png" alt="Logo" width={35} height={35} />
-                    <p className={styles.text}>
-                        Lorem
-                    </p>
-                    <h2 className={styles.author}>
-                        Naruto
-                    </h2>
-                </div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.card}>
-                    <h1 className={styles.title}>
-                        TITLE CARD
-                    </h1>
-                    <Image src="/octopus.png" alt="Logo" width={35} height={35} />
-                    <p className={styles.text}>
-                        Lorem
-                    </p>
-                    <h2 className={styles.author}>
-                        Naruto
-                    </h2>
-                </div>
-                <div className={styles.card}>
-                    <h1 className={styles.title}>
-                        TITLE CARD
-                    </h1>
-                    <Image src="/octopus.png" alt="Logo" width={35} height={35} />
-                    <p className={styles.text}>
-                        Lorem
-                    </p>
-                    <h2 className={styles.author}>
-                        Naruto
-                    </h2>
-                </div>
-                <div className={styles.card}>
-                    <h1 className={styles.title}>
-                        TITLE CARD
-                    </h1>
-                    <Image src="/octopus.png" alt="Logo" width={35} height={35} />
-                    <p className={styles.text}>
-                        Lorem
-                    </p>
-                    <h2 className={styles.author}>
-                        Naruto
-                    </h2>
+                <div className={styles.row}>
+                    <div className={styles.card}>
+                        <h1 className={styles.title}>Table</h1>
+                        <Image src="/octopus.png" alt="Logo" width={35} height={35} />
+                        <p className={styles.text}>
+                            Lorem
+                        </p>
+                        <h2 className={styles.author}>
+                            Naruto
+                        </h2>
+                    </div>
+                    <div className={styles.card}>
+                        <h1 className={styles.title}>Table</h1>
+                        <Image src="/octopus.png" alt="Logo" width={35} height={35} />
+                        <p className={styles.text}>
+                            Lorem
+                        </p>
+                        <h2 className={styles.author}>
+                            Naruto
+                        </h2>
+                    </div>
+                    <div className={styles.card}>
+                        <h1 className={styles.title}>Table</h1>
+                        <Image src="/octopus.png" alt="Logo" width={35} height={35} />
+                        <p className={styles.text}>
+                            Lorem
+                        </p>
+                        <h2 className={styles.author}>
+                            Naruto
+                        </h2>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
   )
 }
