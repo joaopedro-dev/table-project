b8609a2e5ff5d9635aefc696857bb2d276fc2659
João Pedro Barros <jpedro@server01.foursys.local>
2022-05-04 16:09:54 -0300

    alterando estutura

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,7 @@ function Cards(){
             <div className={styles.phrase}>
                 <Image src="/product.png" alt="Logo" width={100} height={100} />
                 <p className={styles.text}>
-                “No meio da dificuldade, esncontra-se a oportunidade”
+                “No meio da dificuldade, encontra-se a oportunidade”
                 </p>
                 <h3 className={styles.author_phrase}>Albert Einstein</h3>
             </div>
