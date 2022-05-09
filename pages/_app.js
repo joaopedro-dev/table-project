a4eca491fe22e1b578649c014f5383ec87f229a3
João Pedro Barros <jpedro@server01.foursys.local>
2022-05-09 13:23:18 -0300

    Adicionando Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,21 @@ import Image from 'next/image'
 function Navbar() {
     return (
         <div className={styles.container}>
-            <div> 
+            <div>
                 <div className={styles.logo}>
                     <Image src="/magic.png" alt="Logo" width={70} height={70} />
                     <p className={styles.item_logo}>
                         Table
                     </p>
-                </div>  
-                    <div>
+                </div>
+                <div>
                     <p className={styles.title_item}>
                         Registrar Ligação
-                    </p>  
-                    </div>
-                    <a className={styles.item}>Cliente</a>
-                    <a className={styles.item}>Terceiro</a>
-                    <a className={styles.item}>Preventivo</a>
+                    </p>
+                </div>
+                <a className={styles.item}>Cliente</a>
+                <a className={styles.item}>Terceiro</a>
+                <a className={styles.item}>Preventivo</a>
             </div>
             <div>
                 <a className={styles.about}>About us</a>
